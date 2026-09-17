9871bb17370d29f30be3afbc873bccff9f87f723
sonono0822 <glutamine0510@gmail.com>
2026-09-17 20:12:41 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C="v2";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./style.css","./app.js","./character.jpeg","./manifest.webmanifest"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
