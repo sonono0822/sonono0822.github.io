a0e0ced504d8189ae740320d013f47dc0d61311b
sonono0822 <glutamine0510@gmail.com>
2026-09-17 20:04:21 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C="v2";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./style.css","./app.js","./character.jpeg","./manifest.webmanifest"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
